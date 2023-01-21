57b30502b3217ca5899c6281b6135f6397f79f82
Alfredoeb9 <alfredoeb96@gmail.com>
2023-01-20 17:36:50 -0800

    [product UI] fixed UI for product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,4 +1,10 @@
 import { client, urlFor } from "../../lib/client";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from "react-icons/ai";
 
 function ProductDetails({ product, products }) {
   const { image, name, details, price } = product;
@@ -8,6 +14,50 @@ function ProductDetails({ product, products }) {
         <div>
           <div className="image-container">
             <img src={urlFor(image && image[0])} />
+          </div>
+          {/* <div className="small-images-container">
+            {image?.map((item, i) => (
+                <img src={urlFor(item)} className="" onMouseEnter="" />
+            ))}
+          </div> */}
+        </div>
+
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">{price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button className="add-to-cart" onClick="" type="button">
+              Add to Cart
+            </button>
+            <button className="buy-now" onClick="" type="button">
+              Buy Now
+            </button>
           </div>
         </div>
       </div>
