f36db287bacf09798fa61425c9c408d3655ea59d
Alfredoeb9 <alfredoeb96@gmail.com>
2023-01-20 17:28:22 -0800

    [product] added dynamic products path with ssg data

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,59 @@
-function ProductDetails() {
-  return <div>Product</div>;
+import { client, urlFor } from "../../lib/client";
+
+function ProductDetails({ product, products }) {
+  const { image, name, details, price } = product;
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image && image[0])} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+// This is needed if you're using getStaticProps so next.js
+// knows what data to prefetch already
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+
+  const products = await client.fetch(query);
+
+  // instantly return an object
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+// pre-render the page at build time using the props/ data returned
+// should use if the data required is available at build time and
+// data comes from a headless cms
+// fetch first product that matches the path slug
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const productsQuery = '*[_type == "product"]';
+
+  const product = await client.fetch(query);
+  const products = await client.fetch(productsQuery);
+
+  return {
+    props: { product, products },
+  };
+};
 
 export default ProductDetails;
